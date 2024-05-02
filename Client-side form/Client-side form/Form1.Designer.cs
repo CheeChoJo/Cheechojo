39be6f0716276452519526804a6a1032fd8678df
Tvojemamanaentou <cipherbill197@gmail.com>
2024-05-02 10:51:48 +0200

    Login page kostra done

---
+++ Client-side form/Client-side form/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Client_side_form
+{
+    partial class CheeChoJoClient
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.PictureBox pictureBoxLogo;
+            this.buttonLogIn = new System.Windows.Forms.Button();
+            this.buttonSignUp = new System.Windows.Forms.Button();
+            pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLogo
+            // 
+            pictureBoxLogo.Image = global::Client_side_form.Properties.Resources.Ciciojo;
+            pictureBoxLogo.Location = new System.Drawing.Point(80, 20);
+            pictureBoxLogo.Name = "pictureBoxLogo";
+            pictureBoxLogo.Size = new System.Drawing.Size(700, 250);
+            pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            pictureBoxLogo.TabIndex = 0;
+            pictureBoxLogo.TabStop = false;
+            // 
+            // buttonLogIn
+            // 
+            this.buttonLogIn.BackColor = System.Drawing.Color.DimGray;
+            this.buttonLogIn.Font = new System.Drawing.Font("Ebrima", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonLogIn.ForeColor = System.Drawing.Color.White;
+            this.buttonLogIn.Location = new System.Drawing.Point(220, 324);
+            this.buttonLogIn.Name = "buttonLogIn";
+            this.buttonLogIn.Size = new System.Drawing.Size(400, 100);
+            this.buttonLogIn.TabIndex = 1;
+            this.buttonLogIn.Text = "Log In";
+            this.buttonLogIn.UseVisualStyleBackColor = false;
+            // 
+            // buttonSignUp
+            // 
+            this.buttonSignUp.BackColor = System.Drawing.Color.DimGray;
+            this.buttonSignUp.Font = new System.Drawing.Font("Ebrima", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonSignUp.ForeColor = System.Drawing.Color.White;
+            this.buttonSignUp.Location = new System.Drawing.Point(220, 430);
+            this.buttonSignUp.Name = "buttonSignUp";
+            this.buttonSignUp.Size = new System.Drawing.Size(400, 100);
+            this.buttonSignUp.TabIndex = 2;
+            this.buttonSignUp.Text = "Sign Up";
+            this.buttonSignUp.UseVisualStyleBackColor = false;
+            // 
+            // CheeChoJoClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(878, 744);
+            this.Controls.Add(this.buttonSignUp);
+            this.Controls.Add(this.buttonLogIn);
+            this.Controls.Add(pictureBoxLogo);
+            this.Name = "CheeChoJoClient";
+            this.Text = "CheeChoJo";
+            this.Load += new System.EventHandler(this.CheeChoJoClient_Load);
+            ((System.ComponentModel.ISupportInitialize)(pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogIn;
+        private System.Windows.Forms.Button buttonSignUp;
+    }
+}
+
---
+++ Client-side form/Client-side form/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_side_form
+{
+    public partial class CheeChoJoClient : Form
+    {
+        public CheeChoJoClient()
+        {
+            InitializeComponent();
+        }
+
+        private void CheeChoJoClient_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
