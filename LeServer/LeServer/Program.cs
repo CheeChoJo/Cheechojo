a81fb6bac3cbdf4fa567e925c44d527015b23759
avealex <alex.avetisyan06@gmail.com>
2024-05-11 21:00:11 +0200

    le server is here

---
+++ LeServer/LeServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+
+async Task<int> GetWebsiteResponseCode(string url)
+{
+    using (var client = new HttpClient())
+    {
+        try
+        {
+            var response = await client.GetAsync(url);
+            return (int)response.StatusCode;
+        }
+        catch (HttpRequestException)
+        {
+            // Handle exceptions (e.g., connection errors)
+            return -1; // Or another value to indicate an error
+        }
+    }
+}
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+async Task<double> GetBakalariValAsync()
+{
+    double bakalariVal = 8.0 / await GetWebsiteResponseCode("https://gateway.gymvod.cz:444/login");
+    return bakalariVal;
+}
+app.MapGet("/", () =>
+{
+    double bakalariVal = GetBakalariValAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+    return bakalariVal;
+});
+
+app.Run();
+
---
+++ LeServer/LeServer/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApiServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
