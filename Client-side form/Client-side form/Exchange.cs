22ae8e565206acdfae30c1f758be9dd11b52c7ef
FilaVila <144894437+FilaVila@users.noreply.github.com>
2024-05-11 18:49:19 +0200

    exchange form

---
+++ Client-side form/Client-side form/Exchange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_side_form
+{
+    public partial class Exchange : Form
+    {
+        public Exchange()
+        {
+            InitializeComponent();
+        }
+    }
+}
