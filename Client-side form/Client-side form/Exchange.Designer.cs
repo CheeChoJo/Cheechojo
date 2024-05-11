3a4be8437634dca9b939c9a8cd840ba31386b31c
FilaVila <144894437+FilaVila@users.noreply.github.com>
2024-05-11 20:27:10 +0200

    exchange form update

--- Client-side form/Client-side form/Exchange.Designer.cs
+++ Client-side form/Client-side form/Exchange.Designer.cs
@@ -32,12 +32,31 @@
             this.labelPrice = new System.Windows.Forms.Label();
             this.labelValue = new System.Windows.Forms.Label();
             this.labelVolume = new System.Windows.Forms.Label();
+            this.labelBalance = new System.Windows.Forms.Label();
+            this.labelTicker1 = new System.Windows.Forms.Label();
+            this.textBoxPrice1 = new System.Windows.Forms.TextBox();
+            this.textBoxVol1 = new System.Windows.Forms.TextBox();
+            this.textBoxValue1 = new System.Windows.Forms.TextBox();
+            this.textBoxBalance = new System.Windows.Forms.TextBox();
+            this.radioButtonBuy = new System.Windows.Forms.RadioButton();
+            this.radioButtonSell = new System.Windows.Forms.RadioButton();
+            this.labelAmountBS = new System.Windows.Forms.Label();
+            this.labelValueBS = new System.Windows.Forms.Label();
+            this.buttonBuySell = new System.Windows.Forms.Button();
+            this.labelTicker2 = new System.Windows.Forms.Label();
+            this.labelTicker3 = new System.Windows.Forms.Label();
+            this.textBoxPrice2 = new System.Windows.Forms.TextBox();
+            this.textBoxPrice3 = new System.Windows.Forms.TextBox();
+            this.textBoxVolume3 = new System.Windows.Forms.TextBox();
+            this.textBoxValue3 = new System.Windows.Forms.TextBox();
+            this.textBoxValu2 = new System.Windows.Forms.TextBox();
+            this.textBoxValu3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // labelTicker
             // 
             this.labelTicker.AutoSize = true;
-            this.labelTicker.Location = new System.Drawing.Point(71, 34);
+            this.labelTicker.Location = new System.Drawing.Point(50, 66);
             this.labelTicker.Name = "labelTicker";
             this.labelTicker.Size = new System.Drawing.Size(51, 20);
             this.labelTicker.TabIndex = 0;
@@ -46,7 +65,7 @@
             // labelPrice
             // 
             this.labelPrice.AutoSize = true;
-            this.labelPrice.Location = new System.Drawing.Point(221, 40);
+            this.labelPrice.Location = new System.Drawing.Point(201, 66);
             this.labelPrice.Name = "labelPrice";
             this.labelPrice.Size = new System.Drawing.Size(44, 20);
             this.labelPrice.TabIndex = 1;
@@ -55,7 +74,7 @@
             // labelValue
             // 
             this.labelValue.AutoSize = true;
-            this.labelValue.Location = new System.Drawing.Point(601, 40);
+            this.labelValue.Location = new System.Drawing.Point(590, 66);
             this.labelValue.Name = "labelValue";
             this.labelValue.Size = new System.Drawing.Size(50, 20);
             this.labelValue.TabIndex = 2;
@@ -65,17 +84,191 @@
             // labelVolume
             // 
             this.labelVolume.AutoSize = true;
-            this.labelVolume.Location = new System.Drawing.Point(390, 40);
+            this.labelVolume.Location = new System.Drawing.Point(378, 66);
             this.labelVolume.Name = "labelVolume";
             this.labelVolume.Size = new System.Drawing.Size(63, 20);
             this.labelVolume.TabIndex = 3;
             this.labelVolume.Text = "Volume";
+            // 
+            // labelBalance
+            // 
+            this.labelBalance.AutoSize = true;
+            this.labelBalance.Location = new System.Drawing.Point(445, 16);
+            this.labelBalance.Name = "labelBalance";
+            this.labelBalance.Size = new System.Drawing.Size(71, 20);
+            this.labelBalance.TabIndex = 4;
+            this.labelBalance.Text = "Balance:";
+            // 
+            // labelTicker1
+            // 
+            this.labelTicker1.AutoSize = true;
+            this.labelTicker1.Location = new System.Drawing.Point(51, 133);
+            this.labelTicker1.Name = "labelTicker1";
+            this.labelTicker1.Size = new System.Drawing.Size(60, 20);
+            this.labelTicker1.TabIndex = 5;
+            this.labelTicker1.Text = "Ticker1";
+            // 
+            // textBoxPrice1
+            // 
+            this.textBoxPrice1.Location = new System.Drawing.Point(184, 133);
+            this.textBoxPrice1.Name = "textBoxPrice1";
+            this.textBoxPrice1.Size = new System.Drawing.Size(80, 26);
+            this.textBoxPrice1.TabIndex = 6;
+            // 
+            // textBoxVol1
+            // 
+            this.textBoxVol1.Location = new System.Drawing.Point(362, 127);
+            this.textBoxVol1.Name = "textBoxVol1";
+            this.textBoxVol1.Size = new System.Drawing.Size(99, 26);
+            this.textBoxVol1.TabIndex = 7;
+            // 
+            // textBoxValue1
+            // 
+            this.textBoxValue1.Location = new System.Drawing.Point(577, 127);
+            this.textBoxValue1.Name = "textBoxValue1";
+            this.textBoxValue1.Size = new System.Drawing.Size(90, 26);
+            this.textBoxValue1.TabIndex = 8;
+            // 
+            // textBoxBalance
+            // 
+            this.textBoxBalance.Location = new System.Drawing.Point(540, 13);
+            this.textBoxBalance.Name = "textBoxBalance";
+            this.textBoxBalance.Size = new System.Drawing.Size(126, 26);
+            this.textBoxBalance.TabIndex = 9;
+            // 
+            // radioButtonBuy
+            // 
+            this.radioButtonBuy.AutoSize = true;
+            this.radioButtonBuy.Location = new System.Drawing.Point(205, 491);
+            this.radioButtonBuy.Name = "radioButtonBuy";
+            this.radioButtonBuy.Size = new System.Drawing.Size(57, 24);
+            this.radioButtonBuy.TabIndex = 10;
+            this.radioButtonBuy.TabStop = true;
+            this.radioButtonBuy.Text = "Buy";
+            this.radioButtonBuy.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonSell
+            // 
+            this.radioButtonSell.AutoSize = true;
+            this.radioButtonSell.Location = new System.Drawing.Point(205, 538);
+            this.radioButtonSell.Name = "radioButtonSell";
+            this.radioButtonSell.Size = new System.Drawing.Size(56, 24);
+            this.radioButtonSell.TabIndex = 11;
+            this.radioButtonSell.TabStop = true;
+            this.radioButtonSell.Text = "Sell";
+            this.radioButtonSell.UseVisualStyleBackColor = true;
+            // 
+            // labelAmountBS
+            // 
+            this.labelAmountBS.AutoSize = true;
+            this.labelAmountBS.Location = new System.Drawing.Point(290, 453);
+            this.labelAmountBS.Name = "labelAmountBS";
+            this.labelAmountBS.Size = new System.Drawing.Size(65, 20);
+            this.labelAmountBS.TabIndex = 12;
+            this.labelAmountBS.Text = "Amount";
+            // 
+            // labelValueBS
+            // 
+            this.labelValueBS.AutoSize = true;
+            this.labelValueBS.Location = new System.Drawing.Point(399, 453);
+            this.labelValueBS.Name = "labelValueBS";
+            this.labelValueBS.Size = new System.Drawing.Size(50, 20);
+            this.labelValueBS.TabIndex = 13;
+            this.labelValueBS.Text = "Value";
+            // 
+            // buttonBuySell
+            // 
+            this.buttonBuySell.Location = new System.Drawing.Point(529, 491);
+            this.buttonBuySell.Name = "buttonBuySell";
+            this.buttonBuySell.Size = new System.Drawing.Size(100, 40);
+            this.buttonBuySell.TabIndex = 14;
+            this.buttonBuySell.Text = "Buy/Sell";
+            this.buttonBuySell.UseVisualStyleBackColor = true;
+            // 
+            // labelTicker2
+            // 
+            this.labelTicker2.AutoSize = true;
+            this.labelTicker2.Location = new System.Drawing.Point(51, 207);
+            this.labelTicker2.Name = "labelTicker2";
+            this.labelTicker2.Size = new System.Drawing.Size(60, 20);
+            this.labelTicker2.TabIndex = 15;
+            this.labelTicker2.Text = "Ticker2";
+            // 
+            // labelTicker3
+            // 
+            this.labelTicker3.AutoSize = true;
+            this.labelTicker3.Location = new System.Drawing.Point(51, 285);
+            this.labelTicker3.Name = "labelTicker3";
+            this.labelTicker3.Size = new System.Drawing.Size(60, 20);
+            this.labelTicker3.TabIndex = 16;
+            this.labelTicker3.Text = "Ticker3";
+            // 
+            // textBoxPrice2
+            // 
+            this.textBoxPrice2.Location = new System.Drawing.Point(183, 203);
+            this.textBoxPrice2.Name = "textBoxPrice2";
+            this.textBoxPrice2.Size = new System.Drawing.Size(80, 26);
+            this.textBoxPrice2.TabIndex = 17;
+            // 
+            // textBoxPrice3
+            // 
+            this.textBoxPrice3.Location = new System.Drawing.Point(184, 283);
+            this.textBoxPrice3.Name = "textBoxPrice3";
+            this.textBoxPrice3.Size = new System.Drawing.Size(79, 26);
+            this.textBoxPrice3.TabIndex = 18;
+            // 
+            // textBoxVolume3
+            // 
+            this.textBoxVolume3.Location = new System.Drawing.Point(366, 203);
+            this.textBoxVolume3.Name = "textBoxVolume3";
+            this.textBoxVolume3.Size = new System.Drawing.Size(94, 26);
+            this.textBoxVolume3.TabIndex = 19;
+            // 
+            // textBoxValue3
+            // 
+            this.textBoxValue3.Location = new System.Drawing.Point(366, 286);
+            this.textBoxValue3.Name = "textBoxValue3";
+            this.textBoxValue3.Size = new System.Drawing.Size(94, 26);
+            this.textBoxValue3.TabIndex = 20;
+            // 
+            // textBoxValu2
+            // 
+            this.textBoxValu2.Location = new System.Drawing.Point(577, 201);
+            this.textBoxValu2.Name = "textBoxValu2";
+            this.textBoxValu2.Size = new System.Drawing.Size(86, 26);
+            this.textBoxValu2.TabIndex = 21;
+            // 
+            // textBoxValu3
+            // 
+            this.textBoxValu3.Location = new System.Drawing.Point(577, 292);
+            this.textBoxValu3.Name = "textBoxValu3";
+            this.textBoxValu3.Size = new System.Drawing.Size(89, 26);
+            this.textBoxValu3.TabIndex = 22;
             // 
             // Exchange
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(729, 612);
+            this.Controls.Add(this.textBoxValu3);
+            this.Controls.Add(this.textBoxValu2);
+            this.Controls.Add(this.textBoxValue3);
+            this.Controls.Add(this.textBoxVolume3);
+            this.Controls.Add(this.textBoxPrice3);
+            this.Controls.Add(this.textBoxPrice2);
+            this.Controls.Add(this.labelTicker3);
+            this.Controls.Add(this.labelTicker2);
+            this.Controls.Add(this.buttonBuySell);
+            this.Controls.Add(this.labelValueBS);
+            this.Controls.Add(this.labelAmountBS);
+            this.Controls.Add(this.radioButtonSell);
+            this.Controls.Add(this.radioButtonBuy);
+            this.Controls.Add(this.textBoxBalance);
+            this.Controls.Add(this.textBoxValue1);
+            this.Controls.Add(this.textBoxVol1);
+            this.Controls.Add(this.textBoxPrice1);
+            this.Controls.Add(this.labelTicker1);
+            this.Controls.Add(this.labelBalance);
             this.Controls.Add(this.labelVolume);
             this.Controls.Add(this.labelValue);
             this.Controls.Add(this.labelPrice);
@@ -94,5 +287,24 @@
         private System.Windows.Forms.Label labelPrice;
         private System.Windows.Forms.Label labelValue;
         private System.Windows.Forms.Label labelVolume;
+        private System.Windows.Forms.Label labelBalance;
+        private System.Windows.Forms.Label labelTicker1;
+        private System.Windows.Forms.TextBox textBoxPrice1;
+        private System.Windows.Forms.TextBox textBoxVol1;
+        private System.Windows.Forms.TextBox textBoxValue1;
+        private System.Windows.Forms.TextBox textBoxBalance;
+        private System.Windows.Forms.RadioButton radioButtonBuy;
+        private System.Windows.Forms.RadioButton radioButtonSell;
+        private System.Windows.Forms.Label labelAmountBS;
+        private System.Windows.Forms.Label labelValueBS;
+        private System.Windows.Forms.Button buttonBuySell;
+        private System.Windows.Forms.Label labelTicker2;
+        private System.Windows.Forms.Label labelTicker3;
+        private System.Windows.Forms.TextBox textBoxPrice2;
+        private System.Windows.Forms.TextBox textBoxPrice3;
+        private System.Windows.Forms.TextBox textBoxVolume3;
+        private System.Windows.Forms.TextBox textBoxValue3;
+        private System.Windows.Forms.TextBox textBoxValu2;
+        private System.Windows.Forms.TextBox textBoxValu3;
     }
 }
