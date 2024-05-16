21d1f2969fb85955a751b46e06075ba72ca4c778
Tvojemamanaentou <104491187+Tvojemamanaentou@users.noreply.github.com>
2024-05-16 13:25:50 +0200

    NO HEAD?!
    spravený záhadný duchohorror tajného errroru ¨"neplatný výraz<<<<<<<HEAD"

--- Client-side form/Client-side form/SignIn.cs
+++ Client-side form/Client-side form/SignIn.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,28 @@ namespace Client_side_form
 
         private void buttonSignIn2_Click(object sender, EventArgs e)
         {
+            string signName = Convert.ToString(textBoxSignName.Text);
+            string signPassword = Convert.ToString(textBoxSignPass1.Text);
+            string signRepeat = Convert.ToString(textBoxSignPass2.Text);
+            string filePath = "C:\\Users\\Public\\Documents\\" + signName + ".txt";
+            if (File.Exists(filePath)) 
+            {
+                MessageBox.Show("Such user already exists, please choose a different username.");
+            }
+            else
+            {
+                if (signPassword == signRepeat)
+                {
+                    MessageBox.Show("Passwords must match");
+                }
+                else 
+                {
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        writer.Write(signPassword);
+                    }
+                }
+            }
             Form Exchange = new Exchange();
             Exchange.Show();
             this.Hide();
