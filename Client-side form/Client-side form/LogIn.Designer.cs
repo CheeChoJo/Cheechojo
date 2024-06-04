ae1a9fcfb0e9be3ff70847517a990b0d752268d1
wendula <144894398+wendula@users.noreply.github.com>
2024-06-04 14:18:31 +0200

    detailiky

--- Client-side form/Client-side form/LogIn.Designer.cs
+++ Client-side form/Client-side form/LogIn.Designer.cs
@@ -37,43 +37,50 @@
             // 
             // textBoxLogName
             // 
-            this.textBoxLogName.Location = new System.Drawing.Point(240, 72);
+            this.textBoxLogName.BackColor = System.Drawing.Color.HotPink;
+            this.textBoxLogName.Location = new System.Drawing.Point(12, 197);
             this.textBoxLogName.Name = "textBoxLogName";
-            this.textBoxLogName.Size = new System.Drawing.Size(214, 26);
+            this.textBoxLogName.Size = new System.Drawing.Size(221, 26);
             this.textBoxLogName.TabIndex = 0;
             // 
             // textBoxLogPass
             // 
-            this.textBoxLogPass.Location = new System.Drawing.Point(240, 150);
+            this.textBoxLogPass.BackColor = System.Drawing.Color.HotPink;
+            this.textBoxLogPass.Location = new System.Drawing.Point(496, 303);
             this.textBoxLogPass.Name = "textBoxLogPass";
-            this.textBoxLogPass.Size = new System.Drawing.Size(233, 26);
+            this.textBoxLogPass.Size = new System.Drawing.Size(221, 26);
             this.textBoxLogPass.TabIndex = 1;
             // 
             // buttonLogIn2
             // 
-            this.buttonLogIn2.Location = new System.Drawing.Point(280, 252);
+            this.buttonLogIn2.BackColor = System.Drawing.Color.DeepPink;
+            this.buttonLogIn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogIn2.Font = new System.Drawing.Font("Bradley Hand ITC", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogIn2.Location = new System.Drawing.Point(237, 404);
             this.buttonLogIn2.Name = "buttonLogIn2";
-            this.buttonLogIn2.Size = new System.Drawing.Size(138, 43);
+            this.buttonLogIn2.Size = new System.Drawing.Size(259, 80);
             this.buttonLogIn2.TabIndex = 2;
             this.buttonLogIn2.Text = "Log In";
-            this.buttonLogIn2.UseVisualStyleBackColor = true;
+            this.buttonLogIn2.UseVisualStyleBackColor = false;
             this.buttonLogIn2.Click += new System.EventHandler(this.buttonLogIn2_Click);
             // 
             // labelLogName
             // 
             this.labelLogName.AutoSize = true;
-            this.labelLogName.Location = new System.Drawing.Point(78, 81);
+            this.labelLogName.Font = new System.Drawing.Font("Bradley Hand ITC", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLogName.Location = new System.Drawing.Point(12, 159);
             this.labelLogName.Name = "labelLogName";
-            this.labelLogName.Size = new System.Drawing.Size(83, 20);
+            this.labelLogName.Size = new System.Drawing.Size(140, 35);
             this.labelLogName.TabIndex = 3;
             this.labelLogName.Text = "Username";
             // 
             // labelLogPass
             // 
             this.labelLogPass.AutoSize = true;
-            this.labelLogPass.Location = new System.Drawing.Point(78, 150);
+            this.labelLogPass.Font = new System.Drawing.Font("Bradley Hand ITC", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLogPass.Location = new System.Drawing.Point(587, 265);
             this.labelLogPass.Name = "labelLogPass";
-            this.labelLogPass.Size = new System.Drawing.Size(78, 20);
+            this.labelLogPass.Size = new System.Drawing.Size(130, 35);
             this.labelLogPass.TabIndex = 4;
             this.labelLogPass.Text = "Password";
             // 
@@ -81,6 +88,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
             this.ClientSize = new System.Drawing.Size(729, 612);
             this.Controls.Add(this.labelLogPass);
             this.Controls.Add(this.labelLogName);
