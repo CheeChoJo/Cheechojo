0927eaf366ddc73fa119847f4e75f6e0cbb9f523
avealex06 <alex.avetisyan06@gmail.com>
2024-06-05 11:12:15 +0200

    mozna funkcni change

--- pythonserver/server.py
+++ pythonserver/server.py
@@ -43,17 +43,35 @@ def get_data():
 def newuser():
     data = request.get_json() #veme json a hodi to do data
     file_name = data.get('accountName')+'.json'
+
     with open(file_name, 'w') as account_file: #vytvori novej file 
         json.dump(data, account_file)
     return '', 201
+
+
+@app.route('/login')
+def login():
+    data = request.get_json()
+    file_name = data.get('accountName')+'.json'
+
+    if file_name():
+        return send_file(file_name, as_attachment=True, mimetype='application/json')
+    else:
+        return "nope"
+
 
 @app.route('/change', methods=['POST', 'GET'])
 def change():
     data = request.get_json()
     try: #pokud existuje soubor
         file_name = data.get('accountName')+'.json'
+        with open(file_name, 'r') as file:
+            orig = json.load(file)
+        for key, value in data.items():
+            if key not in data:  
+                orig[key] = value
         with open(file_name, 'w') as account_file: #tim ze se soubor otevre ve write modu, viz 'w' se vymaze vsechno info v nem
-            json.dump(data, account_file) #dropujeme novy info
+            json.dump(orig, account_file) #dropujeme novy info
         return send_file(file_name, as_attachment=True, mimetype='application/json') #posle soubor zpatky
     except:
         return "no such file"
